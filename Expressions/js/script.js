b2ec14a30a2f5f12ff144a8efd959cd9629c4161
milmc <mamcclelland@fullsail.edu>
2014-12-02 18:36:30 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -6,5 +6,5 @@ var quiz3 = 60;
 var quiz4 = 80;
 
 //the average is the sum of the items divided by the number of items
-var average = quiz1 + quiz2 + quiz3 + quiz4;
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
 console.log(average);
