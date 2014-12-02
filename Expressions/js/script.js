e3f7a568acde5da166d666adba1c74c318560459
milmc <mamcclelland@fullsail.edu>
2014-12-02 17:59:16 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,6 +1,5 @@
-//Expressions
+//Expressions - Age Example
 
-var a = 2; //sets up our variable a and defines it with 2
-var b;
-b = a + 3; //add 2 to a
-console.log(b);
+var yearBorn = 1974;
+var age = 2012 - yearBorn;
+console.log(age);
