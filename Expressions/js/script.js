6132b0831e5a6bd9edfe1f67991959edc5b91143
milmc <mamcclelland@fullsail.edu>
2014-12-02 18:55:18 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,6 +1,7 @@
 //Expressions - Concatenating Strings
 
-var firstName = "Kermit";
-var lastName = "The Frog";
-var fullName = firstName + " " + lastName;
-console.log(fullName);
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
