33d53dbbd9588d93c6a2de75855f873415f31a23
milmc <mamcclelland@fullsail.edu>
2014-12-02 16:36:26 -0500

    valuesAndVariables

--- valuesAndVariables/js/script.js
+++ valuesAndVariables/js/script.js
@@ -3,4 +3,8 @@
 var muppetNames = ["Kermit", "Miss Piggy", "Gonzo" , "Rizzo"]; //Index of 0, 1, 2, 3
 
 muppetNames[1] = "Rizzo";
-console.log(muppetNames[1]);
+muppetNames[2] = "Fozzy"
+
+var num = 2;
+
+console.log(muppetNames[num]);
