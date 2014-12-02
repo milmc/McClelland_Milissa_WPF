e039799d950620c31957a57b7483609c038b9888
milmc <mamcclelland@fullsail.edu>
2014-12-02 18:51:51 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,6 +1,6 @@
-//Expressions - Expressions with Arrays
+//Expressions - Concatenating Strings
 
-var orangeBins = [234, 567, 883];
-
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
+var firstName = "Kermit";
+var lastName = "The Frog";
+var fullName = firstName + lastName;
+console.log(fullName);
