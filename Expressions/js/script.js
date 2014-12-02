1da79c6639a5a47df867228db1bfed9956240860
milmc <mamcclelland@fullsail.edu>
2014-12-02 18:33:09 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,4 +1,9 @@
-//Expressions - Modulo Operator
+//Expressions - PEMDAS - The order of operations
 
-var remainder = 32 % 10;
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+var average = quiz1 + quiz2 + quiz3 + quiz 4;
