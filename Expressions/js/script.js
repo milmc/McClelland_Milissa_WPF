5870a97f77f437d0177a2a2b987910ee0bdd7e85
milmc <mamcclelland@fullsail.edu>
2014-12-02 18:40:56 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -7,4 +7,9 @@ var quiz4 = 80;
 
 //the average is the sum of the items divided by the number of items
 var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-console.log(average);
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
