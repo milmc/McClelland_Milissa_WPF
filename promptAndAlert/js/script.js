5c490d7fafb666b7cef8e407f71a3c4254783f59
milmc <mamcclelland@fullsail.edu>
2014-12-03 16:30:36 -0500

    promptAndAlert

--- promptAndAlert/js/script.js
+++ promptAndAlert/js/script.js
@@ -4,4 +4,4 @@ var width = prompt("We are calculating the area of a rectangle. \nPlease enter t
 var height = prompt("Please enter the height.");
 var area = width * height;
 //console.log(area);
-alert("The area of your rectangle is " + area);
+alert("The area of your rectangle is " + area + " sq feet.");
