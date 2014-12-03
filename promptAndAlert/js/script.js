5069bf081ea7428580e4f2eb884e0dfcac81e54e
milmc <mamcclelland@fullsail.edu>
2014-12-03 16:23:02 -0500

    promptAndAlert

--- promptAndAlert/js/script.js
+++ promptAndAlert/js/script.js
@@ -1,4 +1,4 @@
 //Prompt and Alert
 
-var userInput = prompt("Enter your year of birth:");
-console.log(userInput);
+var width = prompt("We are calculating the area of a rectangle. Please enter the width.");
+var height = prompt("Please enter the height.");
