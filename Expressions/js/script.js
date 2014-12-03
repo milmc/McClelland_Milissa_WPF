30f0a87d80272fa4dd98db6a0e14b5ce5eae303b
milmc <mamcclelland@fullsail.edu>
2014-12-02 19:25:05 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,8 +1,5 @@
-//Casting
+//Expressions - Assignment Operators
 
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-//(407) 203-4529
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+a = a + 4;
+console.log(a);
