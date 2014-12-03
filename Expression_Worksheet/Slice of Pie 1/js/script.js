2203424ed3a2fc6a4e7e6477a1417dbafe30ece3
milmc <mamcclelland@fullsail.edu>
2014-12-03 14:10:12 -0500

    Expression_workSheet
    Slice of pie 1

---
+++ Expression_Worksheet/Slice of Pie 1/js/script.js
@@ -0,0 +1,11 @@
+//Number of slices per person at the party
+
+var partyPeople = 14;
+
+var pizzaOrder = 5;
+
+var slicesPizza = 8 * pizzaOrder;
+
+var result = slicesPizza / partyPeople;
+
+console.log("Each person ate" + " " + result + " " + "slices of pizza a the party.")
