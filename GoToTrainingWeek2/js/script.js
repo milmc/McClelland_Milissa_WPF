3766dbefa6189a0fb2d34e6687dc097f3e81ef47
milmc <mamcclelland@fullsail.edu>
2014-12-03 17:37:27 -0500

    Prompted user name

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -5,4 +5,12 @@
  12-3-2014
  */
 
-alert("testing if the js file is connected.");
+//alert("testing if the js file is connected.");
+
+//Create an age calculator
+
+//Ask the user their name
+var name = prompt ("Please type in your name.");
+
+//Use the variable and say hello to the user
+alert("Welcome " + name + "!" + " Lets find out how old you are.");
