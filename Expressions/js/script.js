00c76e633b08abf88178d58e339e4a0f1d487ba4
milmc <mamcclelland@fullsail.edu>
2014-12-02 19:20:45 -0500

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -3,6 +3,6 @@
 var areaCode = 407;
 var firstPart = 203;
 var secPart = 4529;
-
-var phoneNo = String(areaCode) + String(firstPart) + String(secPart);
+//(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
 console.log(phoneNo);
