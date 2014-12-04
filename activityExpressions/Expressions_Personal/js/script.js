59283f053e45e0df3a5ab75824dd11519b9fe441
milmc <mamcclelland@fullsail.edu>
2014-12-04 16:26:21 -0500

    Created prompt var

---
+++ activityExpressions/Expressions_Personal/js/script.js
@@ -0,0 +1,13 @@
+/**
+ Milissa McClelland
+ SDI Section 04
+ Activity_Expressions - Personal
+ 12-3-2014
+ */
+alert("How to calculate how many days boxes of dog treats will last.");
+
+var numBoxes = prompt("How many boxes of treats did you buy?");
+var amountBox = prompt("How many treats are in one box?");
+var dogTreats = prompt("How many treats do you give your dog a day?");
+var dogs = prompt("How many dogs do you have?");
+
