4c2958b9e4952547db5218e92f0d21ffbea96e54
milmc <mamcclelland@fullsail.edu>
2014-12-04 22:16:04 -0500

    fix names

--- Expression_Worksheet/Discounts/js/script.js
+++ Expression_Worksheet/Discounts/js/script.js
@@ -11,12 +11,12 @@ var discountAmount = prompt("What is the discount?");
 //var itemName = prompt("What is the item?");
 var salesTax = prompt("What is the sales tax?");
 
-var salesAmount = (discountAmount / 100) * originalPrice;
+var salesAmount = discountAmount / 100 * originalPrice;
 
 var salesPrice = originalPrice - salesAmount;
 
 var taxPercent = salesTax / 100 * salesPrice;
 
-var priceTax = taxPercent + Number(salesPrice);
+var priceTax = taxPercent + salesPrice;
 
-console.log(taxPercent);
+console.log(priceTax);
