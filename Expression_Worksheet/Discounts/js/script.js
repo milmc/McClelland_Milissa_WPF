9b4652c94d75ddcfb104003e4a2da0ca20ccd335
milmc <mamcclelland@fullsail.edu>
2014-12-04 22:03:08 -0500

    added math var

--- Expression_Worksheet/Discounts/js/script.js
+++ Expression_Worksheet/Discounts/js/script.js
@@ -8,6 +8,11 @@ expressions worksheet
 
 var originalPrice = prompt("Calculating the price of item. \nWhat is the original price?");
 var discountAmount = prompt("What is the discount?");
-var itemName = prompt("What is the item?");
-var salesTax = prompt("What is the sales tax?");
+//var itemName = prompt("What is the item?");
+//var salesTax = prompt("What is the sales tax?");
 
+var salesAmount = (discountAmount / 100) * originalPrice;
+
+var salesPrice = originalPrice - salesAmount;
+
+console.log(salesPrice);
