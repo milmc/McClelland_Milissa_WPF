e3d815da340c46bd502a26f8d6d46b69ea12fa84
milmc <mamcclelland@fullsail.edu>
2014-12-04 21:45:04 -0500

    var added

---
+++ Expression_Worksheet/Discounts/js/script.js
@@ -0,0 +1,13 @@
+/**
+ Milissa McClelland
+ SDI Section
+expressions worksheet
+ 12-3-2014
+ */
+
+
+var originalPrice = prompt("Calculating the price of item. \nWhat is the original price?");
+var discountAmount = prompt("What is the discount?");
+var itemName = prompt("What is the item?");
+var salesTax = prompt("What is the sales tax?");
+
