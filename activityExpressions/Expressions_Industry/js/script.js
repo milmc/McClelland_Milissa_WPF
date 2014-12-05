02d59330f3124c3e54f9fb72449db3883d042758
milmc <mamcclelland@fullsail.edu>
2014-12-04 20:25:54 -0500

    created alert and explained what is going on

---
+++ activityExpressions/Expressions_Industry/js/script.js
@@ -0,0 +1,10 @@
+/**
+ Milissa McClelland
+ SDI Section 04
+ Activity_Expressions - Industry
+ 12-3-2014
+ */
+
+//Figuring out what to tag an item when it goes on sale.
+//Alerting user what is going on.
+alert("What to tag an item that has gone on sale.")
