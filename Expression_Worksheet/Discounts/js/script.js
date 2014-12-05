a4076c586f60a361341965c4aa7df8e8e116380c
milmc <mamcclelland@fullsail.edu>
2014-12-04 21:30:13 -0500

    divided total by number of grocery bills

--- Expression_Worksheet/Discounts/js/script.js
+++ Expression_Worksheet/Discounts/js/script.js
@@ -9,6 +9,9 @@ var grocerySpent = [150, 89,126,180,117];
 
 var groceryTotal = Number(grocerySpent[0] + Number(grocerySpent[1]) + Number(grocerySpent[2]) +Number(grocerySpent[3]) + Number(grocerySpent[4]));
 
+var aveSpent = groceryTotal / 5;
+
+
 
 
 
