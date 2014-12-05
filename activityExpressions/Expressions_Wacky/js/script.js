d54cf8bbd470399e6e0a4de83fc59a05a165b547
milmc <mamcclelland@fullsail.edu>
2014-12-04 19:45:03 -0500

    Beetle Convention
    created alert for info on what is going on and prompt for how much money

---
+++ activityExpressions/Expressions_Wacky/js/script.js
@@ -0,0 +1,13 @@
+/**
+ Milissa McClelland
+ SDI Section 04
+ Activity_Expressions - Personal
+ 12-3-2014
+ */
+
+alert("The Beetle Convention. \nHow many $5 beetles do you want to buy? \nHow much money will you have left over?");
+
+var beetleMoney = prompt("How much money did you bring to the Beetle Convention?");
+
+
+
