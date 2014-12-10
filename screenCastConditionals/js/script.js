420a93554422674473fc8683a4a2b4c89580bcf6
milmc <mamcclelland@fullsail.edu>
2014-12-09 21:46:00 -0500

    screencastConditionals

--- screenCastConditionals/js/script.js
+++ screenCastConditionals/js/script.js
@@ -6,12 +6,12 @@
  */
 //Conditional logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var payCheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && payCheck > 300){
+if(iPhonePrice < budget || wonLottery){
  console.log("We can buy the phone!");
 }else{
  console.log("No phone for you!!");
