08f1c7a2a9e2ceb3918717f4bf79462c458baa32
milmc <mamcclelland@fullsail.edu>
2014-12-09 20:46:23 -0500

    Animated Airblown® Fire & Ice Dragon

--- screenCastConditionals/js/script.js
+++ screenCastConditionals/js/script.js
@@ -6,13 +6,15 @@
  */
 //Conditional logic - Relation Expressions
 
-var kidHeight = 47;
+var kidHeight = 48;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
  //code preformed if condition is true
  console.log("You can ride the coaster!");
+}else {
+ //code preformed if condition is false
+ console.log("Sorry kid, You've got some growing to do first!");
 }
