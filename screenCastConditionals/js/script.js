cd4177297a79946d9df757da3b914893317895cf
milmc <mamcclelland@fullsail.edu>
2014-12-09 21:42:42 -0500

    screencastConditionals

--- screenCastConditionals/js/script.js
+++ screenCastConditionals/js/script.js
@@ -4,20 +4,15 @@
  Go To Training Week 3
  12-3-2014
  */
-//Conditional logic - else if
+//Conditional logic - Logical Operators
 
-var kidHeight = 48;
-var minHeight = 48;
-var wParentHeight = 45; //the height of the kid with parent
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches in height
-if(kidHeight > minHeight){
- //You can ride
- console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
- //you can ride with a parent present
- console.log("You can ride, but only with a parent present.");
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
+
+//if the price of the phone is less our budget AND if our paycheck is over 300
+if(iPhonePrice < budget && payCheck > 300){
+ console.log("We can buy the phone!");
 }else{
- //sorry you have growing to do
- console.log("Sorry kid, You've got some growing to do first!");
+ console.log("No phone for you!!");
 }
