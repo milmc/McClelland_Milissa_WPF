fff1a3f2da1600d91772cd7a46b5de2d0bee58b1
milmc <mamcclelland@fullsail.edu>
2014-12-09 21:23:30 -0500

    screencastConditionals

--- screenCastConditionals/js/script.js
+++ screenCastConditionals/js/script.js
@@ -4,17 +4,20 @@
  Go To Training Week 3
  12-3-2014
  */
-//Conditional logic - Relation Expressions
+//Conditional logic - else if
 
 var kidHeight = 48;
 var minHeight = 48;
-
+var wParentHeight = 45; //the height of the kid with parent
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
- //code preformed if condition is true
+ //You can ride
  console.log("You can ride the coaster!");
-}else {
- //code preformed if condition is false
+}else if(kidHeight > wParentHeight){
+ //you can ride with a parent present
+ console.log("You can ride, but only with a parent present.");
+}else{
+ //sorry you have growing to do
  console.log("Sorry kid, You've got some growing to do first!");
 }
