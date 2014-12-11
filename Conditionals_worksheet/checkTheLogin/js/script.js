96019c874039e722a8f31b2962a2c65f8ca295cd
milmc <mamcclelland@fullsail.edu>
2014-12-10 23:35:24 -0500

    added comments

--- Conditionals_worksheet/checkTheLogin/js/script.js
+++ Conditionals_worksheet/checkTheLogin/js/script.js
@@ -7,15 +7,22 @@
 
 //Check the Login
 
+//created prompt for username and password var and var for the actual username and password
 var inputUN = prompt("What is your username?");
 var inputPW = prompt("What is your password?");
 var userName = "Godzilla54";
 var password = "Mothra64";
 
+//created if statement to see if username and password entered match the var
 if(inputUN === userName && inputPW === password){
+ //logged to console welcome if BOTH username and password are correct
  console.log("Welcome, " + userName + "!");
+//created else if to see if var inputUN and userName are different but inputPW and password are the same
 }else if(inputUN != userName && inputPW === password){
+ //logged to console user not found if username is wrong but password is right
  console.log("User not found. Try again.");
+//created else for a wrong password
 }else{
+ //logged to console passwords don't match
  console.log("Password does not match our records.");
 }
