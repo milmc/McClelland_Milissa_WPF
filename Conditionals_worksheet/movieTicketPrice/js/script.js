58b9fff8f87e61103a5e1b4e2b64481d9b7e9037
milmc <mamcclelland@fullsail.edu>
2014-12-10 23:52:15 -0500

    created more var

--- Conditionals_worksheet/movieTicketPrice/js/script.js
+++ Conditionals_worksheet/movieTicketPrice/js/script.js
@@ -10,3 +10,8 @@
 alert("Movie Ticket Price!");
 var age = prompt("How old are you?");
 var time = prompt("What time does the movie start?");
+
+var normalTicket = "$12.00";
+var senior10 = "$7.00";
+var matinee = "$7.00";
+
