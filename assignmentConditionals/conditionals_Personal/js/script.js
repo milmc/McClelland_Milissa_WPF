0ccce13ef250b06d14be5cc730aacc9f77c898a0
milmc <mamcclelland@fullsail.edu>
2014-12-11 21:40:04 -0500

    created var

---
+++ assignmentConditionals/conditionals_Personal/js/script.js
@@ -0,0 +1,18 @@
+/**
+ Milissa McClelland
+ SDI Section 04
+ Assignment_Conditionals
+ 12-3-2014
+ */
+
+//Can you go shopping for yourself after buying groceries and putting money into savings
+
+
+var budget = prompt("What is your budget?");
+var groceries = prompt("How much did you spend on groceries?");
+var savings =  prompt("How much did you put into savings?");
+
+
+
+
+
---
+++ assignmentConditionals/conditionals_Wacky/js/script.js
@@ -0,0 +1,8 @@
+/**
+ Milissa McClelland
+ SDI Section 04
+ Assignment_Conditionals
+ 12-3-2014
+ */
+
+//
