517ce34bcfa00cb2499dcce18096d9159a74882c
milmc <mamcclelland@fullsail.edu>
2014-12-11 22:36:45 -0500

    created console and alert

--- assignmentConditionals/conditionals_Industry/js/script.js
+++ assignmentConditionals/conditionals_Industry/js/script.js
@@ -15,3 +15,6 @@ var employee = prompt("Are you an employee?");
 
 if(employee === "yes"){
  var employPrice = originalPrice - (.2*originalPrice);
+ console.log("Your item for $" + originalPrice + " is 20% off and is now $" + employPrice + ".");
+ alert("Your item for $" + originalPrice + " gets an employee discount and is 20% off. Is now $" + employPrice + ".");
+}
