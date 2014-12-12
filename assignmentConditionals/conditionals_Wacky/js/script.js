1bf6b2e6c00afa57b9ddc07777feeefdb5307c14
milmc <mamcclelland@fullsail.edu>
2014-12-11 22:05:46 -0500

    added comments

--- assignmentConditionals/conditionals_Wacky/js/script.js
+++ assignmentConditionals/conditionals_Wacky/js/script.js
@@ -7,9 +7,13 @@
 
 //Will the dragon egg hatch!
 
+//created var prompts for egg age and if it is cold or warm and assigned to var
 var eggAge = prompt("How many days old is the dragon egg?");
 var warmCold = prompt("Is the dragon egg warm or cold?");
-
+//created var hatch
 var hatch;
+//created ternary that determines if egg age > 90 and if it is warm, it logs it will hatch. if not it logs it will not hatch
 hatch = (eggAge > 90 && warmCold === "warm") ? "Your dragon egg will hatch soon!" : "Your dragon egg is still growing!";
 console.log(hatch);
+//lets the user know if their egg will hatch or not
+alert(hatch);
