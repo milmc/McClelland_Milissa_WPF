4d5c5d8f789a6487192c9cbe8b013467716a8965
milmc <mamcclelland@fullsail.edu>
2014-12-11 22:35:32 -0500

    created var

--- assignmentConditionals/conditionals_Industry/js/script.js
+++ assignmentConditionals/conditionals_Industry/js/script.js
@@ -1,3 +1,13 @@
 /**
- * Created by milissamcclelland on 12/11/14.
+ Milissa McClelland
+ SDI Section 04
+ Assignment_Conditionals
+ 12-3-2014
  */
+
+//Do you qualify for a discount
+
+alert("Do you get to have a discount on your item?");
+var originalPrice = prompt("How much is the item?");
+var employee = prompt("Are you an employee?");
+
