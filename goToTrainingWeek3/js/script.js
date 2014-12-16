5ca6aa3aa9f64f936a2c56cf685b5b89ab11540b
milmc <mamcclelland@fullsail.edu>
2014-12-16 16:11:41 -0500

    tested first fruit in the bowl array

--- goToTrainingWeek3/js/script.js
+++ goToTrainingWeek3/js/script.js
@@ -25,3 +25,18 @@ console.log(bowlOfFruit.length);
 //What is the last index number of the array, using the length property?
 //Last index number is ALWAYS one less than the length
 //bowlOfFruit.length - 1  is the last index number
+
+//create a var to track the number of pears we have
+var totalNumPears = 0;
+
+//Test each item and see if it is a pear
+//if(condition to test){code to run if true}
+if(bowlOfFruit[0] === "pear"){
+    //this will run if true
+    //add one to our counting var
+    console.log("This fruit is a pear!");
+    totalNumPears++;
+}else{
+    //this code will run if the if statement is FALSE!
+    console.log("This fruit not a pear!");
+}
