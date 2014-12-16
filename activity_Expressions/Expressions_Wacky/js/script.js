a1a284a98593eea63742cd300c924be4524d6de3
milmc <mamcclelland@fullsail.edu>
2014-12-16 15:57:37 -0500

    setup fruit array
    setup fruit array

--- activity_Expressions/Expressions_Wacky/js/script.js
+++ activity_Expressions/Expressions_Wacky/js/script.js
@@ -1,7 +1,7 @@
 /**
  Milissa McClelland
  SDI Section 04
- Activity_Expressions - Wacky
+ Activity_Expressions - Wackyx
  12-3-2014
  */
 
---
+++ goToTrainingWeek3/js/script.js
@@ -0,0 +1,19 @@
+/*
+Milissa McClelland
+SDI Section #4
+Go To Training Week #3
+12/10/2014
+ */
+
+//lalert("Testing to make sure we are working.");
+//Create code that sorts through of fruit names and picks out pears
+//create our "fruit bowl" array
+
+var bowlOfFruit = ["apple", "banana", "pear", "peach"];
+//console logs the WHOLE array
+console.log(bowlOfFruit);
+
+//How can I get one fruit inside of the bowl
+console.log(bowlOfFruit[1]);
+
+
