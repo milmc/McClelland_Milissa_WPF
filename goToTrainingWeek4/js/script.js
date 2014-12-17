3401f82a805bbc647df76f6a66a592224c43fae3
milmc <mamcclelland@fullsail.edu>
2014-12-17 17:16:57 -0500

    setup for week 4

---
+++ goToTrainingWeek4/js/script.js
@@ -0,0 +1,13 @@
+/*
+Milissa McClelland
+SDI sec 1412
+Go To Training Week 4
+12/17/2014
+ */
+
+//alert("Testing to see if the js file is connected");
+
+//Create a function that spits a random integer between two given values
+
+//Ask the user for a min number
+var min = prompt("Let's get a random number between two values. \nPlease type in the minimum value.");
--- screencastFunctions/js/script.js
+++ screencastFunctions/js/script.js
@@ -4,11 +4,11 @@ Screencast:Functions
 SDI sec 04
  */
 
-function dogYears(age){//<---parameters
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + " years old.");
-}
-var age1 = 4;
-dogYears(age1);//<---arguments
-dogYears(5);
+var total = calcArea(30, 20);
 
+function calcArea(w,h){
+    var area = w * h;
+    return area;//function spitting the info out
+}
+
+console.log(total);
