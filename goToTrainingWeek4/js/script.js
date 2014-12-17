de97e9bdac8ac39a12e5fb82f4fb619f21711a48
milmc <mamcclelland@fullsail.edu>
2014-12-17 18:07:18 -0500

    finished week 4

--- goToTrainingWeek4/js/script.js
+++ goToTrainingWeek4/js/script.js
@@ -65,3 +65,36 @@ if(userGoodMood === "yes" || userGoodMood === "Yes"){
 }else{
     Console.log("Sorry to hear that. \nMaybe you should code more, that should make you happy!")
 }
+
+
+//Ask user for a max value
+var max = prompt ("Please type in a maximum value.");
+
+//validate the prompt
+while(max === " || isNaN(max){
+    //re-prompt the user
+    max = prompt("Please do not leave blank and olny use numbers");
+
+}
+
+//parameters are used to catch the incoming arguments
+function randomizr(minNum,maxNum){
+    //in the function
+    console.log("Inside the function");
+    // use parameters instead of the main code variables
+    //Find a random number between 2 values
+    //Math.random() * (max value - min value) + min value
+    var randomNum = Math.round(Math.random() * (maxNum - minNum) + Number(minNum));
+    //console.log(randomNum); DO NOT USE CONSOLE.LOG INSIDE OF FUNCTION!!
+    // use a return value instead
+    return randomNum;
+}
+//we need a function call to "run" the function
+//arguments go in the function call
+//"What information  in our main does our function need access to?"
+
+//we have to "catch" the returned value of our functional call
+var results = randomizr(min,max);
+//we use this results var in our final console.log
+console.log("The random number is " + results);
+console.log(randomizr(30,70));//can use other arguments
