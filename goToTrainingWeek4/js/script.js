9773f1e901dca267af00698d2d1eb20174ea767f
milmc <mamcclelland@fullsail.edu>
2014-12-17 17:32:55 -0500

    prompted min value and validated with while loop

--- goToTrainingWeek4/js/script.js
+++ goToTrainingWeek4/js/script.js
@@ -11,3 +11,33 @@ Go To Training Week 4
 
 //Ask the user for a min number
 var min = prompt("Let's get a random number between two values. \nPlease type in the minimum value.");
+
+//validate that the user actually typed in something, so not blank
+
+/*
+//older way of doing it
+if(min===""){
+    //reprompt the user
+    min = prompt("Please do not leave blank \nPlease type in a min number!");
+}*/
+
+//validate using a while loop
+//test to see if blank
+/*
+while(min===""){
+    min = prompt("Please do not leave blank! \nPlease type in the minimum value.");
+
+}
+//validate that it is a number using a while loop
+while(isNaN(min)){
+    //reprompt the user
+    min = prompt("Please only type in numbers. \nPlease type in a min number.");
+
+}
+*/
+
+//validate using a combined while loop
+while(min === "" || isNaN(min)){
+    //re-prompt the user
+    min = prompt("Please do not leave blank and only type in numbers. \nPlease type in a min value");
+}
