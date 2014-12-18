a25646f177406bab3a4d3509f9029f00e3b19a47
milmc <mamcclelland@fullsail.edu>
2014-12-18 16:18:44 -0500

    created var

--- Functions_Worksheet/Stung!/js/script.js
+++ Functions_Worksheet/Stung!/js/script.js
@@ -4,3 +4,9 @@
 Functions Worksheet - Stung!
  12/17/2014
  */
+
+//How to calculate how many stings to kill an animal using a function in javascript
+
+
+var result = stingWeight(prompt("How much does the animal weigh in pounds?"));
+
