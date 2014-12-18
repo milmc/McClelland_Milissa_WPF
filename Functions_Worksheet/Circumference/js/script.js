b6b78a300ff0ace8abc77a1c6c4af11876689ee7
milmc <mamcclelland@fullsail.edu>
2014-12-18 12:46:53 -0500

    worksheet functions files created

---
+++ Functions_Worksheet/Circumference/js/script.js
@@ -0,0 +1,6 @@
+/*
+ Milissa McClelland
+ SDI sec 1412
+ Functions Worksheet - Circumference
+ 12/17/2014
+ */
---
+++ Functions_Worksheet/Stung!/js/script.js
@@ -0,0 +1,6 @@
+/*
+ Milissa McClelland
+ SDI sec 1412
+Functions Worksheet - Stung!
+ 12/17/2014
+ */
