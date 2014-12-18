e8dfa4d6131ef25d32ac205ffe87dd8cce5ebc59
milmc <mamcclelland@fullsail.edu>
2014-12-18 11:47:26 -0500

    while loop

--- screencastLoops/js/script.js
+++ screencastLoops/js/script.js
@@ -4,6 +4,11 @@ SDI sec 4
 Loops - While Loops
  */
 
+var b = 10//sets up the index
 
+while(b > 0){//checks the condition
+    console.log(b + " kegs on the wall.");
+    b--;//increments or decrements the index
+}
 
 
