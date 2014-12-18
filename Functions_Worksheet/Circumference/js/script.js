0d68e3b17b936a3b91c533da3286f00e0b4dea8c
milmc <mamcclelland@fullsail.edu>
2014-12-18 16:08:35 -0500

    added comments

--- Functions_Worksheet/Circumference/js/script.js
+++ Functions_Worksheet/Circumference/js/script.js
@@ -4,14 +4,19 @@
  Functions Worksheet - Circumference
  12/17/2014
  */
+//How to find the circumference of a circle using a javascript function.
 
 
+//creates a place to store the return with "var result" and sets the argument for the function
 var result = calcCir(prompt("What is the radius of the circle?"));
+//sets the functions parameter
 function calcCir(r){
+ //var to store the math to find the circumference of a circle
  var circumference = 2 * Math.PI * r;
+ //spits out the information to the outside of the function and stores it in var result
  return circumference;
 }
-
+//logs to console the results of the function
 console.log("The circumference of the circle is: " + result);
 
 
