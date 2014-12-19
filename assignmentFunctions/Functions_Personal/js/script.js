c067795201f6b22ef805a5c2e999346b2ebb4120
milmc <mamcclelland@fullsail.edu>
2014-12-18 20:27:57 -0500

    alert user

--- assignmentFunctions/Functions_Personal/js/script.js
+++ assignmentFunctions/Functions_Personal/js/script.js
@@ -5,4 +5,13 @@
  12-10-2014
  */
 
+//How much the interest for the price of a car will be, can you afford it with what you already have, or do you need a loan
+
+alert("How much is the interest of your car loan.");
+
+
+
+
+
+
 
